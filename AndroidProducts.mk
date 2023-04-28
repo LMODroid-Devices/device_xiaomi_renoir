c22d82db07ecc6ca69fa4d2d04eb046dec08bb0e
LMO GerritBot <gerrit@libremobileos.com>
2023-04-28 04:12:49 +0000

    Adapt to LMODroid by device-add script
    Change-Id: I8983e9d1278dffbc3e4c34e86d688ee7c63b42d4

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_renoir.mk
+    $(LOCAL_DIR)/lmodroid_renoir.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_renoir-user \
-    lineage_renoir-userdebug \
-    lineage_renoir-eng
+    lmodroid_renoir-user \
+    lmodroid_renoir-userdebug \
+    lmodroid_renoir-eng
